8c9bfbc7f62d46662a927710daf4c3d77e9e238a
Eric Siebeneich <eric.siebeneich@gmail.com>
2018-11-14 23:18:23 -0600

    log the individual commands

--- scripts/publish.js
+++ scripts/publish.js
@@ -10,11 +10,11 @@ git.status((err, status) => {
 
 readline.question('Commit message: ', (msg) => {
   git.checkout('master')
-    .merge('develop')
-    .add('./*')
-    .commit(msg)
-    .push('origin', 'master')
-    .checkout('develop');
+    .merge('develop', (err, data) => { console.error(err); console.log(data); })
+    .add('./*', (err, data) => { console.error(err); console.log(data); })
+    .commit(msg, (err, data) => { console.error(err); console.log(data); })
+    .push('origin', 'master', (err, data) => { console.error(err); console.log(data); })
+    .checkout('develop', (err, data) => { console.error(err); console.log(data); });
 
   readline.close();
 });
