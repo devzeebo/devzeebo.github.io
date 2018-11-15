9fa709afefdd27ee73aed8d242bec689a06fcfb9
Eric Siebeneich <eric.siebeneich@gmail.com>
2018-11-15 00:43:14 -0600

    logging meaningful

--- scripts/publish.js
+++ scripts/publish.js
@@ -22,31 +22,31 @@ async function publishToMaster() {
 
   const commitMessage = await Prompt('Commit message: ');
   await git.stash({ '--include-untracked': null });
-  await Prompt('Press Enter');
+  await Prompt('Post stash/Pre checkout master');
   await git.checkout('master');
-  await Prompt('Press Enter');
+  await Prompt('Post checkout master/pre pull o m');
   await git.pull('origin', 'master', { '-XTheirs': null });
-  await Prompt('Press Enter');
+  await Prompt('post pull o m/ pre merge develop');
   try {
     await git.merge(['develop', '-Xtheirs', '-m', commitMessage]);
     // eslint-disable-next-line
   } finally { }
-  await Prompt('Press Enter');
+  await Prompt('post merge/pre rimraf');
 
   // keeping dist and index because we need them
   // keeping node_modules because we don't want to reinstall every time
   // keeping .git so we don't blow away the repo
   rimraf.sync('!(dist*|index.html|node_modules*|.git*)');
 
-  await Prompt('Press Enter');
+  await Prompt('post rimraf/pre pop');
   await git.stash('pop');
-  await Prompt('Press Enter');
+  await Prompt('post pop/pre add');
   await git.add('./*');
-  await Prompt('Press Enter');
+  await Prompt('post add/pre amend');
   await git.commit(commitMessage, [], { '--amend': null });
-  await Prompt('Press Enter');
+  await Prompt('post amend/pre push');
   await git.push('origin', 'master');
-  await Prompt('Press Enter');
+  await Prompt('post push/pre checkout develop');
   await git.checkout('develop');
 
   readline.close();
