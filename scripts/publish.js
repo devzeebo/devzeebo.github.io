f8a33874b499cda14ecc5e4bc6ca0abb0a3bb413
Eric Siebeneich <eric.siebeneich@gmail.com>
2018-11-14 23:14:19 -0600

    Testing publish

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,9 +1,18 @@
 // eslint-disable-next-line
 const git = require('simple-git')();
+const readline = require('readline').createInterface({ input: process.stdin, output: process.stdout });
 
 git.status((err, status) => {
-  console.log(status);
   if (status.modified.length) {
     throw new Error('Develop has uncommitted changes');
   }
+});
+
+readline.question('Commit message: ', (msg) => {
+  git.checkout('master')
+    .merge('develop')
+    .add('./*')
+    .commit(msg)
+    .push('origin', 'master')
+    .checkout('develop');
 });
