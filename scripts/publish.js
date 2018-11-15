86d4370102a0960db3573b7708bb86d59a7e37e4
Eric Siebeneich <eric.siebeneich@gmail.com>
2018-11-14 23:27:08 -0600

    calling each git func in turn

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line
 const git = require('simple-git')();
+// eslint-disable-next-line
 const rimraf = require('rimraf');
 const readline = require('readline').createInterface({ input: process.stdin, output: process.stdout });
 
@@ -10,18 +11,17 @@ git.status((err, status) => {
 });
 
 readline.question('Commit message: ', (msg) => {
-  git
-    .stash('--include-untracked', (err, data) => { console.error(err); console.log(data); })
-    .checkout('master', (err, data) => { console.error(err); console.log(data); })
-    .merge('develop', '-Xtheirs', (err, data) => { console.error(err); console.log(data); });
+  git.stash('--include-untracked', (err, data) => { console.error(err); console.log(data); });
+  git.checkout('master', (err, data) => { console.error(err); console.log(data); });
+  git.merge('develop', '-Xtheirs', (err, data) => { console.error(err); console.log(data); });
 
   rimraf.sync('dist');
   rimraf.sync('index.html');
 
-  git.stash('pop', (err, data) => { console.error(err); console.log(data); })
-    .commit(msg, (err, data) => { console.error(err); console.log(data); })
-    .push('origin', 'master', (err, data) => { console.error(err); console.log(data); })
-    .checkout('develop', (err, data) => { console.error(err); console.log(data); });
+  git.stash('pop', (err, data) => { console.error(err); console.log(data); });
+  git.commit(msg, (err, data) => { console.error(err); console.log(data); });
+  git.push('origin', 'master', (err, data) => { console.error(err); console.log(data); });
+  git.checkout('develop', (err, data) => { console.error(err); console.log(data); });
 
   readline.close();
 });
