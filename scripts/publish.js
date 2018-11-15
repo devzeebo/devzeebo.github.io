f960405cf33cf468f8a060c1f6a2c64252112cbf
Eric Siebeneich <eric.siebeneich@gmail.com>
2018-11-15 00:57:13 -0600

    Simplified

--- scripts/publish.js
+++ scripts/publish.js
@@ -20,31 +20,24 @@ async function publishToMaster() {
     throw new Error('Develop has uncommitted changes');
   }
 
-  const commitMessage = await Prompt('Commit message: ');
   await git.stash({ '--include-untracked': null });
   await Prompt('Post stash/Pre checkout master');
   await git.checkout('master');
   await Prompt('Post checkout master/pre pull o m');
   await git.pull('origin', 'master', { '-XTheirs': null });
-  await Prompt('post pull o m/ pre merge develop');
-  try {
-    await git.merge(['develop', '-Xtheirs', '-m', commitMessage]);
-    // eslint-disable-next-line
-  } finally { }
-  await Prompt('post merge/pre rimraf');
+  await Prompt('Post checkout master/pre rimraf');
 
-  // keeping dist and index because we need them
   // keeping node_modules because we don't want to reinstall every time
   // keeping .git so we don't blow away the repo
-  rimraf.sync('!(dist*|index.html|node_modules*|.git*)', { glob: { dot: true, nosort: true, silent: true } });
+  rimraf.sync('!(node_modules*|.git*)', { glob: { dot: true, nosort: true, silent: true } });
 
   await Prompt('post rimraf/pre pop');
   await git.stash('pop');
   await Prompt('post pop/pre add');
   await git.add('./*');
-  await Prompt('post add/pre amend');
-  await git.commit(commitMessage, [], { '--amend': null });
-  await Prompt('post amend/pre push');
+  const commitMessage = await Prompt('Commit message: ');
+  await git.commit(commitMessage);
+  await Prompt('post commit/pre push');
   await git.push('origin', 'master');
   await Prompt('post push/pre checkout develop');
   await git.checkout('develop');
