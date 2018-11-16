e47dc6ee5cf24ce72ffe22b1649159eecb9d210a
Eric Siebeneich <eric.siebeneich@gmail.com>
2018-11-15 23:12:26 -0600

    Ignore posts folder during rimraf

--- scripts/publish.js
+++ scripts/publish.js
@@ -26,7 +26,7 @@ async function publishToMaster() {
 
   // keeping node_modules because we don't want to reinstall every time
   // keeping .git so we don't blow away the repo
-  rimraf.sync('!(node_modules*|.git*|.build*)', { glob: { dot: true, nosort: true, silent: true } });
+  rimraf.sync('!(node_modules*|.git*|.build*|posts*)', { glob: { dot: true, nosort: true, silent: true } });
   fs.renameSync('.build/dist', 'dist');
   fs.renameSync('.build/index.html', 'index.html');
   rimraf.sync('.build', { glob: { dot: true, silent: true } });
