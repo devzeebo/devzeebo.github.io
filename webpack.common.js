b17e7c29b18db415271c9350981f584beccf38ba
Eric Siebeneich <eric.siebeneich@gmail.com>
2018-11-15 22:52:27 -0600

    Moved output to .build/dist

--- webpack.common.js
+++ webpack.common.js
@@ -10,7 +10,7 @@ module.exports = {
     pathinfo: true,
     path: path.resolve(__dirname, 'dist'),
     filename: 'blog.[contenthash].js',
-    publicPath: 'dist',
+    publicPath: '.build/dist',
   },
   optimization: {
     minimizer: [
